a77bddafd87e235c3a2e974236df8cff34a1e07b
Dan Cuomo <dacuo@microsoft.com>
2019-07-13 11:15:14 -0700

    adding build files for merge

---
+++ Tests/build/setup/install.ps1
@@ -0,0 +1,57 @@
+git clone -q https://github.com/PowerShell/DscResource.Tests
+
+Import-Module -Name "$env:APPVEYOR_BUILD_FOLDER\DscResource.Tests\AppVeyor.psm1"
+Invoke-AppveyorInstallTask
+
+[string[]]$PowerShellModules = @("Pester", 'posh-git', 'psake', 'poshspec', 'PSScriptAnalyzer')
+
+$ModuleManifest = Test-ModuleManifest .\$($env:RepoName).psd1 -ErrorAction SilentlyContinue
+$repoRequiredModules = $ModuleManifest.RequiredModules.Name
+
+if ($repoRequiredModules) { $PowerShellModules += $repoRequiredModules }
+
+# This section is taken care of by Invoke-AppVeyorInstallTask
+<#[string[]]$PackageProviders = @('NuGet', 'PowerShellGet')
+
+# Install package providers for PowerShell Modules
+ForEach ($Provider in $PackageProviders) {
+    If (!(Get-PackageProvider $Provider -ErrorAction SilentlyContinue)) {
+        Install-PackageProvider $Provider -Force -ForceBootstrap -Scope CurrentUser
+    }
+}#>
+
+# Install the PowerShell Modules
+ForEach ($Module in $PowerShellModules) {
+    If (!(Get-Module -ListAvailable $Module -ErrorAction SilentlyContinue)) {
+        Install-Module $Module -Scope CurrentUser -Force -Repository PSGallery
+    }
+    
+    Import-Module $Module
+}
+
+<#
+    Feature Installation
+#>
+
+$serverFeatureList = 'RSAT-Clustering-Powershell', 'Hyper-V'
+
+$BuildSystem = Get-CimInstance -ClassName 'Win32_OperatingSystem'
+
+Switch -Wildcard ($BuildSystem.Caption) {
+    '*Windows 10*' {
+        Write-Output "Not Implemented"
+        Write-Output "Not Implemented"
+        Write-Output "Not Implemented"
+        Write-Output 'Build System is Windows 10'
+        # Get FailoverCluster Capability Name
+        $capabilityName = (Get-WindowsCapability -Online | Where-Object Name -like *RSAT*FailoverCluster.Management*).Name
+        Add-WindowsCapability -Name $capabilityName -Online
+    }
+
+    Default {
+        Write-Output 'Build System is Windows 2016/2019'
+        Install-WindowsFeature -Name $serverFeatureList -IncludeManagementTools
+
+        Get-WindowsFeature | ? InstallState -eq 'Installed' | ft -AutoSize        
+    }
+}
---
+++ Tests/build/unit/unit.build.tests.ps1
@@ -0,0 +1,27 @@
+Describe "$($env:repoName)-Manifest" {
+    $DataFile   = Import-PowerShellDataFile .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
+    $TestModule = Test-ModuleManifest       .\$($env:repoName).psd1 -ErrorAction SilentlyContinue
+
+    Context Manifest-Validation {
+        It "[Import-PowerShellDataFile] - $($env:repoName).psd1 is a valid PowerShell Data File" {
+            $DataFile | Should Not BeNullOrEmpty
+        }
+
+        It "[Test-ModuleManifest] - $($env:repoName).psd1 should pass the basic test" {
+            $TestModule | Should Not BeNullOrEmpty
+        }
+
+        Import-Module .\$($env:repoName).psd1 -Force -WarningAction SilentlyContinue -ErrorAction SilentlyContinue
+        $command = Get-Command $($env:repoName) -ErrorAction SilentlyContinue
+
+        It "Should have the $($env:repoName) function available" {
+            $command | Should not BeNullOrEmpty
+        }
+    }
+
+    <#
+    Context Validate-GlobalExamples {
+        Validate-DCB -ExampleConfig NDKm1 -TestScope Global
+    }
+    #>
+}
